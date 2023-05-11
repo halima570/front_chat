4dfa1702c67eb5bc8a6e6352b0a369a2ab6f4843
halima570 <84968500+halima570@users.noreply.github.com>
2023-05-11 19:24:48 +0100

    adding front

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./chat";
 
-const socket = io.connect("https://backend-chat-9xnn.onrender.com/");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
